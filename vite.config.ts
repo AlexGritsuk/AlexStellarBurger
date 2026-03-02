8b9e12c4fac446e81786f932265bc413550ecf90
AlexGritsuk <alexey1212@inbox.ru>
2026-03-02 20:36:44 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/https://github.com/AlexGritsuk/AlexStellarBurger/",
+  base: "/AlexStellarBurger/",
   plugins: [react()],
 });
